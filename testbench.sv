d5f2b6abd665b8c7d7656586738594a75ad05826
fuanruisu <fuanruisu@github.com>
2025-04-06 14:27:22 -0600

    running base test

--- testbench.sv
+++ testbench.sv
@@ -11,6 +11,7 @@ import uvm_pkg::*; // contains all uvm base classes
 module top;
   
   logic clk;
+  logic rst;
   
   //..instantiation of lower modules.. Mainly DUT,Interface
   
@@ -19,14 +20,14 @@ module top;
     .rst(intf.rst),
     .buf_in(intf.buf_in),
     .buf_out(intf.buf_out),
-    .wr_en (intf.clk),
-    .rd_en(intf.rst),
+    .wr_en (intf.wr_en),
+    .rd_en(intf.rd_en),
     .buf_empty (intf.buf_empty),
     .buf_full(intf.buf_full),
     .fifo_counter (intf.fifo_counter)
   );
   
-  fifo_intf intf(.clk(clk)); //(.interface clk(topmodule clk))
+  fifo_intf intf(.clk(clk), .rst(rst)); //(.interface clk(topmodule clk))
   
   initial begin 
     uvm_config_db#(virtual fifo_intf)::set(null,"*","vif",intf);
@@ -34,8 +35,8 @@ module top;
   
   initial  begin
     clk = 0;
-    //rst = 1;
-    //#20 rst = 0;
+    rst = 1;//nota: drivear el reset en el seq item hace que la primer seq se pierda, como haciamos en los proyectos para drivear el reset? I mean podiamos resetear durante la simulacion para terminos de PM
+    #1 rst = 0;
   end
     always #10 clk = ~clk;
    
@@ -44,11 +45,11 @@ module top;
      //enable wave dump
     $dumpfile("dump.vcd"); 
     $dumpvars;
-    //#100 $finish;//uncomment to dump waveform
+   // #300 $finish;//uncomment to dump waveform
   end
    
   initial begin 
-    run_test("fifo_test");
+    run_test("fifo_base_test");
   end
   
 endmodule
