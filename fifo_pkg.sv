c596ba3c16528a25ea424c33352e7786f64d189d
fuanruisu <fuanruisu@github.com>
2025-04-06 14:27:55 -0600

    Adding new test files

--- fifo_pkg.sv
+++ fifo_pkg.sv
@@ -1,13 +1,16 @@
-`define BUF_WIDTH 3    // BUF_SIZE = 16 -> BUF_WIDTH = 4, no. of bits to be used in pointer
-`define BUF_SIZE ( 1<<`BUF_WIDTH )
+`define BUFF_WIDTH 4    // BUF_SIZE = 16 -> BUF_WIDTH = 4, no. of bits to be used in pointer
+`define BUFF_SIZE ( 1<<`BUFF_WIDTH )
 
 `include "interface.sv"
 `include "seq_item.sv"
-`include "sequence.sv"
+`include "sequence_lib.sv"
 `include "sequencer.sv"
 `include "driver.sv"
 `include "monitor.sv"
 `include "agent.sv"
 `include "scoreboard.sv"
 `include "environment.sv"
-`include "test.sv"
+`include "fifo_base_test.sv"
+`include "wr_rd_test.sv"
+`include "wr_full_test.sv"
+
