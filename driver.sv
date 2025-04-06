eba7f11a67e27ca4d6bfdfdfa620441a2b029f38
fuanruisu <fuanruisu@github.com>
2025-04-06 14:25:04 -0600

    Adding input signals to drive to dut

--- driver.sv
+++ driver.sv
@@ -3,7 +3,7 @@ class fifo_driver extends uvm_driver#(fifo_seq_item);
   `uvm_component_utils(fifo_driver) // reg class to uvm factory
   
   virtual fifo_intf intf;
-  fifo_seq_item tx;
+  //fifo_seq_item tx;
   
   //standard constructor
   function new(string name ="fifo_driver", uvm_component parent);
@@ -23,16 +23,19 @@ class fifo_driver extends uvm_driver#(fifo_seq_item);
     forever begin
       `uvm_info("driver Class", "run_phase", UVM_MEDIUM)
 
-      seq_item_port.get_next_item(tx);
-      drive(tx);
+      seq_item_port.get_next_item(req);
+      drive(req);//req is the default handle for seq_item declared
       seq_item_port.item_done();
     end
   endtask
   
   task drive(fifo_seq_item tx);
-    @(posedge intf.clk)
-    intf.rst <= tx.rst;
-    //intf.d <= tx.d;
-    //intf.q <= tx.q;
+    //sending tx to dut through interface
+   @(posedge intf.clk);
+    //intf.rst <= tx.rst;
+    intf.wr_en <= tx.wr_en;
+    intf.rd_en <= tx.rd_en;
+    intf.buf_in <= tx.buf_in;
+    
   endtask
 endclass
